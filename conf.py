61f51adb2b7a538a1212283d98b862402556e33a
Wes Turner <wes@wrd.nu>
2012-11-14 11:04:18 -0600

    Updated conf.py: Commenting out intersphinx_mapping for now

--- conf.py
+++ conf.py
@@ -286,4 +286,4 @@ epub_copyright = u'2012, Wes Turner'
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+# intersphinx_mapping = {'http://docs.python.org/': None}
