80e17dbd0c36eb045a2315b43b9ece455e6002bb
Wes Turner <wes@wrd.nu>
2012-11-16 09:45:19 -0600

    Updated setup.py: Adding install_requires and extras_require sections

--- setup.py
+++ setup.py
@@ -3,6 +3,29 @@ import sys, os
 
 version = '0.2'
 
+install_requires = [
+    'PasteDeploy',
+    'networkx',
+]
+
+docs_extras = [
+    'docutils',
+    'pygments',
+    'Sphinx',
+    'Sphinx-PyPi-upload',
+    'sphinxfeed',
+    'changelog',
+    'sphinxcontrib-mercurial',
+    'sphinx-git',
+    'sphinxcontrib-bitbucket',
+    'sphinxcontrib-issuetracker',
+    'sphinxcontrib-cheeseshop',
+    'sphinxcontrib-bibtex',
+    'sphinxcontrib-paverutils',
+    'github-tools[template]',
+]
+
+
 setup(name='techw',
       version=version,
       description="techw",
@@ -17,12 +40,15 @@ TODO""",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=True,
-      install_requires=[
-          # -*- Extra requirements: -*-
-          'PasteDeploy',
-      ],
+      install_requires=install_requires,
+      extras_require={
+        "docs": docs_extras,
+      },
       entry_points="""
       # -*- Entry points: -*-
+      [console_scripts]
+      rst_shift = techw.rst_shift:main
+      rst_outline = techw.rst_slideoutline:main
       [paste.app_factory]
       main = techw.wsgiapp:make_app
       """,
