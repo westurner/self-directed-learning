cd2d0ee987b6f97689349dce56548c57347f3db5
Wes Turner <wes@wrd.nu>
2012-11-16 09:45:19 -0600

    Updated rst_shift.py:

--- techw/rst_shift.py
+++ techw/rst_shift.py
@@ -5,57 +5,42 @@ from __future__ import print_function
 ReStructuredText Utility for modifying ReStructuredText headings by the
 specified shift offset.
 
-For example::
-
-    ==========
-    Title
-    ==========
-
-    Heading 1
-    =========
-    Heading 2
-    ---------
-    Heading 3
-    ~~~~~~~~~
-
-Shifted by 1::
-
-    Title
-    ======
-    Heading 1
-    ----------
-    Heading 2
-    ~~~~~~~~~~
-    Heading 3
-    '''''''''
-
 """
 import re
 import optparse
 import logging
+from collections import namedtuple, OrderedDict
+from StringIO import StringIO
 
-RE_TITLE = re.compile(r'^([\=|\-|\~]+)\n(.*)\n^([\=|\-|\~]+)\n', re.MULTILINE)
-RE_HEADING = re.compile(r'^(?:[\=|\-|\~]?)(?:[\n]?)(.*)\n^([\=|\-|\~]+)\n',
+RGX_TITLE = re.compile(r'^([\=|\-|\~]+)\n(.*)\n^([\=|\-|\~]+)\n', re.MULTILINE)
+RGX_HEADING = re.compile(r'^(?:[\=|\-|\~]?)(?:[\n]?)(.*)\n^([\=|\-|\~]+)\n',
                                                                  re.MULTILINE)
-STANDARD_HEADINGS = {
- 0: '==',
- 1: '=',
- 2: '-',
- 3: '~',
- 4: "'",
- 5: '"',
- 6: '`',
- 7: '^',
- 8: '_',
- 9: '*',
- 10: '+',
- 11: '#',
- 12: '<',
- 13: '>'}
+
+
+RGX_TITLE = re.compile(r'^([\=|\-|\~]+)\n(.*)\n^([\=|\-|\~]+)\n', re.MULTILINE)
+RGX_HEADING = re.compile(r'^(?:[\=|\-|\~]?)(?:[\n]?)(.*)\n^([\=|\-|\~]+)\n',
+                                                                 re.MULTILINE)
+
+STANDARD_HEADINGS = OrderedDict((
+(0, '=='),
+(1, '='),
+(2, '-'),
+(3, '~'),
+(4, "'"),
+(5, '"'),
+(6, '`'),
+(7, '^'),
+(8, '_'),
+(9, '*'),
+(10, '+'),
+(11, '#'),
+(12, '<'),
+(13, '>'),
+))
 
 log = logging.getLogger()
 
-def rst_shift(input_file, shiftby):
+def rst_shift(input_file, shiftby, shift_title=True):
     """
 
     Shift the headings of a restructuredtext document
@@ -66,106 +51,222 @@ def rst_shift(input_file, shiftby):
     :type shiftby: signed int
     """
 
-    shiftfactor = shiftby
-
-
     f = input_file # open(input_file,'r+')
     lines = f.read()
 
-
     depthcount = 0
-    headings = {}
+    headings = OrderedDict()
+    log.debug("document headings: %s" % headings)
     sections = []
 
     # Special case for title
-    title_obj = RE_TITLE.search(lines)
+    title_obj = RGX_TITLE.search(lines)
 
-    (overline, title, underline) = (
-            hasattr(title_obj,'groups') and title_obj.groups()
-            or ('=','','='))
-    #if len(overline) == len(underline):
-    headings[overline[0]*2] = 0
+    (overline, title, underline) = ('=','','=')
+    title = None
+    if title_obj:
+        (overline, title, underline) = title_obj.groups()
+        if not len(overline) == len(underline):
+            log.error( (overline, title, underline) )
+            raise Exception("is this a malformed title?")
+        else:
+            underline_char = overline[0]*2 # Because it is a header
+            headings[underline_char] = 0
+            log.debug("document headings: %s" % headings)
 
-    #print 0, title
-    log.debug(title)
-    #sections.append((0, '\n'.join([overline, title, underline])))
+
+            #print 0, title
+            log.debug(title)
+            text = title
+            depth = 0
+            log.debug("%d\t%s%s" % (depth, depth * '   ', text))
+            sections.append((0, underline_char, '\n'.join([overline, title, underline])))
 
     # Extract section headings and existing section heading numbering
-    for g in RE_HEADING.finditer(lines):
+
+    # for each heading in the document
+    for g in RGX_HEADING.finditer(lines):
         (text, line) = g.groups()
+        if text in title and line == underline:
+            continue # TODO
         underline_char = line[0]
+        # check if this heading type is already in the document
         depth = headings.get(underline_char)
         if not depth:
             depthcount += 1
             headings[underline_char] = depthcount
+            depth = depthcount
+        #log.debug(depth)
 
         heading_level = headings[underline_char]
         sections.append(
             (heading_level, underline_char, '\n'.join((text, line))))
         #print heading_level,
-        log.debug("%s%s" % (heading_level * '   ', text))
+        log.debug("%d\t%s%s" % (depth, heading_level * '   ', text))
+
+    log.debug("document headings: %s" % headings)
 
     STD_HEADINGS = STANDARD_HEADINGS.values()
 
-    # Expand headings with elements from STD_HEADINGS
-    for char in sorted(headings.keys()):
-        STD_HEADINGS.remove(char)
-    for n in xrange(len(STD_HEADINGS)):
-        dest = depthcount + ((((shiftby > 0) and 1 or -1)) * n)
-        headings[STD_HEADINGS[n]] = dest
+    log.debug( ' '.join(STD_HEADINGS))
+
 
     # TODO
+    # Expand headings with elements from STD_HEADINGS
+    #for char in sorted(headings.keys()):
+    #    STD_HEADINGS.remove(char)
+    # TODO
     headings_by_n = dict(
-        (x[1], x[0]) for x in headings.items(),
+        (x[1], x[0]) for x in headings.iteritems(),
     )
+
+    for n in xrange(len(STD_HEADINGS)):
+        dest = depthcount + n # ((((shiftby > 0) and 1 or -1)) * n)
+        headings[STD_HEADINGS[n]] = dest
+        log.debug("document headings: %s" % headings)
+
+    log.debug("document headings: %s" % headings)
+    log.debug( ' '.join(STD_HEADINGS))
+    log.debug( ' '.join(str(x) for x in headings_by_n.values()))
+
+
+    # TODO
 
     #output = copy.copy(lines)
     output = lines
 
     # Perform underline replacements
     for section in sections:
-        (level, char, text) = section
-        newlevel = level + shiftby
-        newchar = headings_by_n[newlevel]
-        section, underline = text.split('\n',1)
-        newtext = '\n'.join((section, underline.replace(char, newchar)))
+        (depth, char, text) = section
+        newdepth = depth + shiftby
+        newchar = headings_by_n[newdepth]
+        newtext = None
 
+        section, underline = text.split('\n',1)
+
+        # special case for title header
+        if shiftby != 0:
+            if depth == 0 and len(char) > 1:
+                char = char[0]
+                newchar = newchar[0]
+            elif depth == 1 and text.startswith('\n'):
+                newtext = '\n'
+
+
+        newtext = newtext or '\n'.join((section, underline.replace(char, newchar)))
+
+        log.error( (depth, newdepth, text, newtext) )
         output = output.replace(text, newtext, 1)
 
     return output
 
+TestTuple = namedtuple('TestTuple', ('input','shiftkey','output',))
+
+# (input,
+
+_RST_TEST_INPUT_1="""
+=====
+Title
+=====
+title_content
+Heading 1
+=========
+Heading 1.1
+-----------
+Heading 1.1.1
+~~~~~~~~~~~~~
+Heading 2
+=========
+"""
+
+RST_TESTS = (
+    TestTuple(_RST_TEST_INPUT_1, 0, _RST_TEST_INPUT_1),
+    TestTuple(_RST_TEST_INPUT_1, 1,"""
+
+Title
+=====
+title_content
+Heading 1
+---------
+Heading 1.1
+~~~~~~~~~~~
+Heading 1.1.1
+\"\"\"\"\"\"\"\"\"\"\"\"\"
+Heading 2
+---------
+"""),
+)
+
+#RST_TEST_DICT = OrderedDict(
+#    ((t.input,t.shiftkey), t.output) for t in RST_TESTS
+#)
+
+import itertools
+def _compare_test_output(_input, _output, expected_output):
+    formatstr="%-20s %-20s %-20s"
+
+    log.error("%-20s %-20s %-20s" % ("input", "output", "expected output"))
+    log.error("="*60)
+    for n in itertools.izip_longest(
+                _input.split('\n'),
+                _output.split('\n'),
+                expected_output.split('\n')):
+        log.error("%-20s %-20s %-20s" % (n))
+
 
 import unittest
 class Test_rst_shift(unittest.TestCase):
-    def test_rst_shift(self):
-        pass
+    def test_rst_1(self):
+        for test_input, shift, expected_output in RST_TESTS:
+            input_ = StringIO(test_input)
+            output = rst_shift(input_, shift)
+            try:
+                self.assertEqual(expected_output, output)
+            except Exception, e:
+                log.exception(e)
+                log.error("# shift = %r" % shift)
+                _compare_test_output(test_input, output, expected_output)
+                pass
+               #raise
+        raise Exception()
 
 
 def main():
     import optparse
     import logging
+    import sys
 
-    prs = optparse.OptionParser(usage="./%prog : args")
+    prs = optparse.OptionParser(
+            usage="./%prog : -s <int> -i <input_file> -o <output_file>",
+            description="Shift reStructuredText headings by -s")
 
-    prs.add_option('-i','--input-file',
-                    dest='input_file',
-                    action='store',
-                    help='ReStructuredText file to reindent')
     prs.add_option('-s','--shiftby',
                     dest='shiftby',
                     action='store',
                     default=0,
                     help='Heading shift factor (-5, 5)')
+    prs.add_option('-i','--input-file',
+                    dest='input_file',
+                    action='store',
+                    help='Input ReStructuredText file to shift headings')
+    prs.add_option('-o','--output-file',
+                    dest='output_file',
+                    action='store',
+                    default=sys.stdout,
+                    help='Output file to shift headings')
 
     prs.add_option('-v', '--verbose',
                     dest='verbose',
-                    action='store_true',)
+                    action='store_true',
+                    help="Show debugging output")
     prs.add_option('-q', '--quiet',
                     dest='quiet',
-                    action='store_true',)
+                    action='store_true',
+                    help="Disable logging")
     prs.add_option('-t', '--test',
                     dest='run_tests',
-                    action='store_true',)
+                    action='store_true',
+                    help="Run unit tests.")
 
     (opts, args) = prs.parse_args()
 
@@ -189,16 +290,21 @@ def main():
 
     if opts.input_file == '-':
         opts.input_file = sys.stdin
-    else:
+    elif opts.input_file is not None:
         opts.input_file = open(opts.input_file,'r+')
 
-    # TODO?
-    print( rst_shift(opts.input_file, int(opts.shiftby)) )
-
-
+        opts.output_file.write(
+            rst_shift(opts.input_file, int(opts.shiftby))
+        )
+    else:
+        if args:
+            log.error("Could not parse commandline arguments: %r" % args)
+        prs.print_help()
+        exit(1)
 
 if __name__ == "__main__":
     main()
+
 
 
 
